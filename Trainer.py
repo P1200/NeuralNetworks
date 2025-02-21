6bb95199a487aee2b9f0815fdc11d62f6024e769
P1200 <sekwoja5@gmail.com>
2025-02-21 19:25:27 +0100

    Remove validation dataset and data augmentation.

--- Trainer.py
+++ Trainer.py
@@ -5,21 +5,19 @@ class Trainer:
     def __init__(self, device):
         self.device = device
 
-    def train_validate_test(self, model, train_loader, val_loader, test_loader, criterion, optimizer, num_epochs):
+    def train_and_test(self, model, train_loader, test_loader, criterion, optimizer, num_epochs):
         loss_history = []
         accuracy_history = []
 
         for epoch in range(num_epochs):
             avg_train_loss = self.train(criterion, model, optimizer, train_loader)
 
-            avg_val_loss, val_accuracy = self.validate(criterion, model, val_loader)
-
             avg_test_loss, test_accuracy = self.test(criterion, model, test_loader)
 
-            loss_history.append((avg_train_loss, avg_val_loss, avg_test_loss))
-            accuracy_history.append((val_accuracy, test_accuracy))
+            loss_history.append((avg_train_loss, avg_test_loss))
+            accuracy_history.append(test_accuracy)
             print(f"Epoch [{epoch + 1}/{num_epochs}] - "
-                  f"Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%, "
+                  f"Train Loss: {avg_train_loss:.4f}, "
                   f"Test Accuracy: Test Accuracy: {test_accuracy:.2f}%")
         return loss_history, accuracy_history
 
@@ -42,26 +40,6 @@ class Trainer:
         avg_test_loss = test_loss / len(test_loader)
         test_accuracy = 100 * correct / total
         return avg_test_loss, test_accuracy
-
-    def validate(self, criterion, model, val_loader):
-        model.eval()
-        val_loss = 0.0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in val_loader:
-                images, labels = images.to(self.device), labels.to(self.device)
-
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-                val_loss += loss.item()
-
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        avg_val_loss = val_loss / len(val_loader)
-        val_accuracy = 100 * correct / total
-        return avg_val_loss, val_accuracy
 
     def train(self, criterion, model, optimizer, train_loader):
         model.train()
--- single_layer_network/SingleLayer.py
+++ single_layer_network/SingleLayer.py
@@ -1,12 +1,10 @@
 import time
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import PolLettDB.PolLettDB as pld
-from Augmenter import Augmenter
 from ChartDrawer import ChartDrawer
 from Trainer import Trainer
 from single_layer_network.ImageDataset import ImageDataset
@@ -15,23 +13,6 @@ from single_layer_network.SingleLayerNN import SingleLayerNN
 INPUT_SIZE = 64 * 64
 NUM_CLASSES = 80
 
-
-def random_split_data(X, y, split_sizes):
-    assert sum(split_sizes) == 1.0, "Sum must be 100%"
-
-    # 🔀 Losowe permutowanie indeksów (gwarantuje, że dane i etykiety pozostaną zgodne)
-    indices = np.random.permutation(len(X))
-    X, y = X[indices], y[indices]
-
-    # Obliczenie rozmiarów zbiorów
-    train_size = int(len(X) * split_sizes[0])
-    val_size = int(len(X) * split_sizes[1])
-
-    # Podział na trzy zbiory
-    X_train, X_val, X_test = np.split(X, [train_size, train_size + val_size])
-    y_train, y_val, y_test = np.split(y, [train_size, train_size + val_size])
-
-    return (X_train, y_train), (X_val, y_val), (X_test, y_test)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = SingleLayerNN(INPUT_SIZE, NUM_CLASSES).to(device)
@@ -51,29 +32,15 @@ label_mapping = {label: idx for idx, label in enumerate(unique_labels)}
 mapped_labels = np.array([label_mapping[label] for label in raw_labels])
 labels = torch.tensor(mapped_labels, dtype=torch.long)
 
-(train_data, train_labels), (val_data, val_labels), (test_data, test_labels) = (
-    random_split_data(images, labels, [0.7, 0.15, 0.15]))
+images = np.expand_dims(images, axis=1)
+images = torch.tensor(images, dtype=torch.float32)
 
-# augmenter = Augmenter()
-# augmented_train_data, augmented_train_labels = augmenter.augment_dataset(train_data, train_labels)
-augmented_train_data = train_data
-augmented_train_labels = train_labels
+dataset = ImageDataset(images, labels)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
 
-train_images = np.expand_dims(augmented_train_data, axis=1)
-train_images = torch.tensor(train_images, dtype=torch.float32)
-
-val_images = np.expand_dims(val_data, axis=1)
-val_images = torch.tensor(val_images, dtype=torch.float32)
-
-test_images = np.expand_dims(test_data, axis=1)
-test_images = torch.tensor(test_images, dtype=torch.float32)
-
-train_dataset = ImageDataset(train_images, augmented_train_labels)
-val_dataset = ImageDataset(val_images, val_labels)
-test_dataset = ImageDataset(test_images, test_labels)
-
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 criterion = nn.CrossEntropyLoss()
@@ -82,9 +49,9 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 trainer = Trainer(device)
 
 start_time = time.time()
-loss_history, accuracy_history = trainer.train_validate_test(model, train_loader, val_loader, test_loader, criterion,
-                                                             optimizer,
-                                                             num_epochs=200)
+loss_history, accuracy_history = trainer.train_and_test(model, train_loader, test_loader, criterion,
+                                                        optimizer,
+                                                        num_epochs=200)
 end_time = time.time()
 total_time = end_time - start_time
 
